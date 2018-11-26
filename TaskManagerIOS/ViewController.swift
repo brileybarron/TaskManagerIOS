de75f62e81bb7be693aa779e3523ef6874296211
Eric Barron <pericb@hotmail.com>
2018-11-26 12:20:16 -0600

    Initial Commit

---
+++ TaskManagerIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagerIOS
+//
+//  Created by Briley Barron on 11/26/18.
+//  Copyright © 2018 Briley Barron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
