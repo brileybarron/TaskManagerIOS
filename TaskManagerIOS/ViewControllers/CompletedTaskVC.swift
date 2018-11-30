85dc59389c83e4119b6c86a8058d8405e30d9a6d
Eric Barron <pericb@hotmail.com>
2018-11-30 10:27:52 -0600

    made it so the cells are not user interaction enabled

--- TaskManagerIOS/ViewControllers/CompletedTaskVC.swift
+++ TaskManagerIOS/ViewControllers/CompletedTaskVC.swift
@@ -10,28 +10,13 @@ import UIKit
 
 class CompletedTaskVC: UIViewController {
     @IBOutlet weak var completedTasksTableView: UITableView!
-    @IBOutlet weak var completedSearchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-extension CompletedTaskVC: UITableViewDelegate, UITableViewDataSource {
+extension CompletedTaskVC: UITableViewDelegate, UITableViewDataSource {//gives a table view with only the completed tasks
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Library.library.completed.count
     }
@@ -42,6 +27,7 @@ extension CompletedTaskVC: UITableViewDelegate, UITableViewDataSource {
         cell.task = Library.library.completed[indexPath.row]
         cell.nameLabel.text = Library.library.completed[indexPath.row].name
         cell.setup()
+        cell.isUserInteractionEnabled = false
         return cell
     }
     
--- TaskManagerIOS/ViewControllers/IncompleteTasksVC.swift
+++ TaskManagerIOS/ViewControllers/IncompleteTasksVC.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class IncompleteTasksVC: UIViewController {
     @IBOutlet weak var incompleteTasksTableView: UITableView!
-    @IBOutlet weak var incompleteSearchBar: UISearchBar!
     
     var editedTasks = Library.library.notComplete
     var textEdited = [Task]()
@@ -33,37 +32,7 @@ class IncompleteTasksVC: UIViewController {
     */
 
 }
-extension IncompleteTasksVC: UISearchBarDelegate {
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            let search = searchText.lowercased().replacingOccurrences(of: " ", with: "-")
-            var tempArray = [Task]()
-            
-            
-            if search == "" {
-                // display everything
-                textEdited = editedTasks
-                tempArray = []
-            } else {
-                
-                
-                
-                for name in editedTasks {
-                    if name.name.contains(search) {
-                        print(name)
-                        tempArray.append(name)
-                    }
-                }
-                
-                textEdited.removeAll()
-                textEdited.append(contentsOf: tempArray)
-                tempArray.removeAll()
-            }
-            
-            incompleteTasksTableView.reloadData()
-        }
-    }
-
-extension IncompleteTasksVC: UITableViewDelegate, UITableViewDataSource {
+extension IncompleteTasksVC: UITableViewDelegate, UITableViewDataSource {//gives a table view with only incomplete tasks
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return editedTasks.count
     }
@@ -74,6 +43,7 @@ extension IncompleteTasksVC: UITableViewDelegate, UITableViewDataSource {
         cell.task = editedTasks[indexPath.row]
         cell.nameLabel.text = editedTasks[indexPath.row].name
         cell.setup()
+        cell.isUserInteractionEnabled = false
         return cell
     }
     
